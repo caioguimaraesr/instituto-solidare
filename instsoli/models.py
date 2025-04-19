efcdaf197b76c3a25895556fa583298b90de9263
rafaferraz05 <rafaelferraz.silva05@gmail.com>
2025-04-19 14:07:33 -0300

    Criando models para turmas

--- instsoli/models.py
+++ instsoli/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from usuario.models import InformacoesPessoais
 # Create your models here.
 class Curso(models.Model):
     nome = models.CharField(max_length=200)
@@ -9,3 +9,13 @@ class Curso(models.Model):
     
     def __str__(self):
         return self.nome
+
+class Turma(models.Model):
+    nome = models.CharField(max_length=100)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+    alunos = models.ManyToManyField(InformacoesPessoais, blank=True)
+
+    def _str_(self):
+        return f'{self.nome} - {self.curso.nome}'
