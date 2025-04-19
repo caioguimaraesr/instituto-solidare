3fd51080937e5c299c6d4f5b26cd78c364a47058
rafaferraz05 <rafaelferraz.silva05@gmail.com>
2025-04-19 12:10:57 -0300

    criando a url para aba do portal do professor e das turmas

--- instsoli/urls.py
+++ instsoli/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('cursos/', views.cursos, name='cursos'),
     path('cursos/<int:id>/', views.detalhe_curso, name='detalhe_curso'),
+    path('portal_professor/', views.portal_professor,name='portal_professor'),
+    path('portal_professor/turmas/', views.gerenciar_turmas,name='gerenciar_turmas'),
+    
 ]
