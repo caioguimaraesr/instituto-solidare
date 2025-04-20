936299ae6646132d67491892b797ac833066cb26
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-04-20 18:09:16 -0300

    adicionando as urls para as turmas

--- instsoli/urls.py
+++ instsoli/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('portal_professor/', views.portal_professor,name='portal_professor'),
     path('portal_professor/turmas/', views.gerenciar_turmas,name='gerenciar_turmas'),
     path('portal_professor/turmas/criar/', views.criar_turma,name='criar_turma'),
+    path('turmas/excluir/<int:id>/', views.excluir_turma, name='excluir_turma'),
+    path('turmas/editar/<int:id>/', views.editar_turma, name='editar_turma'),
 ]
