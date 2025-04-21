46c40306951f48b08918cb5af7a1cb4c48ec7fa4
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-04-21 02:12:34 -0300

    criando os novos models

--- instsoli/models.py
+++ instsoli/models.py
@@ -11,7 +11,7 @@ class Curso(models.Model):
         return self.nome
 
 class Turma(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=200)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
     data_inicio = models.DateField()
     data_fim = models.DateField()
@@ -19,3 +19,15 @@ class Turma(models.Model):
 
     def _str_(self):
         return f'{self.nome} - {self.curso.nome}'
+    
+class Frequencia(models.Model):
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+    aluno = models.ForeignKey(InformacoesPessoais, on_delete=models.CASCADE)
+    data = models.DateField()
+    presente = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('turma', 'aluno', 'data')
+
+    def __str__(self):
+        return f"{self.aluno.user.username} - {self.data} - {'Presente' if self.presente else 'Faltou'}"
