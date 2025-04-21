06ec852d0735b5b3267878cf0aa14e509256d6f3
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-04-21 02:12:17 -0300

    adicionado os novos models no /admin

--- instsoli/admin.py
+++ instsoli/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Curso
+from .models import Curso, Frequencia, Turma
 # Register your models here.
 @admin.register(Curso)
 class CursoAdmin(admin.ModelAdmin):
     ...
 
-    
+@admin.register(Frequencia)
+class FrequenciaAdmin(admin.ModelAdmin):
+    ...  
+
+@admin.register(Turma)
+class TurmaAdmin(admin.ModelAdmin):
+    ...  
