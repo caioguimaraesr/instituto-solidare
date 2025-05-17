eb3b91e3d488f22928cbae3c2d9675f8ce9fd55e
rafaferraz05 <rafaelferraz.silva05@gmail.com>
2025-05-16 21:33:58 -0300

    criando teste automatizados

--- create_curso.py
+++ create_curso.py
@@ -12,12 +12,9 @@ descricao = 'Descrição para o curso de programação'
 novo_curso = Curso.objects.create(
     nome=nome,
     descricao=descricao,
+    imagem=None,
+    documento=None
 )
 
-# Cypress.Commands.add('createCurso', () => {
-#   cy.exec('python create_curso.py', { failOnNonZeroExit: false });
-# });
 
-# beforeEach(() => {
-#    cy.createCurso();
-# });
+
