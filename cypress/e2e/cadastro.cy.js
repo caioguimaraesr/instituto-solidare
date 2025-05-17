24a5a4e794d2af586aaa66a4a4fbcaf86e328453
rafaferraz05 <rafaelferraz.silva05@gmail.com>
2025-05-17 13:01:05 -0300

    teste de automacao do login

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -51,6 +51,41 @@ describe('teste de cadastro', () => {
         cy.get('#curso').select('Iniciação a Programação');
         cy.get('.form-step.active > .btn-2').click();
     });
+
+    it('login', () => {
+        cy.visit('/auth/register/');
+        cy.get('#username').type('usuario teste');
+        cy.get('#email').type('teste@gmail.com');
+        cy.get('#password').type('123');
+        cy.get('#confirm_password').type('123');
+        cy.get('#pergunta').select('Aluno');
+        cy.get('.next-btn > .bx').click();
+        cy.get('#first_name').type('teste');
+        cy.get('#last_name').type('teste2');
+        cy.get('#CPF').type('11111111111');
+        cy.get('#telefone').type('11941500444');
+        cy.get('#data_nascimento').type('2005-11-05');
+        cy.get('#genero').select('Masculino');
+        cy.get('.next-btn > .bx').click();
+        cy.get('#CEP').type('54521150');
+        cy.get('#endereco').type('rua do teste');
+        cy.get('#numero').type('120');
+        cy.get('#bairro').type('bairro do teste');
+        cy.get('#estado').type('estado do teste');
+        cy.get('#estado').type('estado do teste');
+        cy.get('#cidade').type('cidade do teste');
+        cy.get('.next-btn > .bx').click();
+        cy.get('#escolaridade').select('Fundamental Completo');
+        cy.get('#escola').type('escola teste');
+        cy.get('#turno').select('Tarde');
+        cy.get('#curso').select('Iniciação a Programação');
+        cy.get('.form-step.active > .btn-2').click();
+
+        cy.get('#username').type('usuario teste');
+        cy.get('#password').type('123');
+        cy.get('.btn-2').click();
+
+    });
     
     afterEach(() => {
         cy.deleteAllCurso();
