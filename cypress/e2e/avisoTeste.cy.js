0471361c63ad76f1a06353ad88efe303fcb52590
rafaferraz05 <rafaelferraz.silva05@gmail.com>
2025-05-17 14:08:19 -0300

    aviso teste

---
+++ cypress/e2e/avisoTeste.cy.js
@@ -0,0 +1,78 @@
+Cypress.Commands.add('registerUser', () => {
+    cy.visit('/auth/register/');
+    cy.get('#username').type('usuario teste');
+    cy.get('#email').type('teste@gmail.com');
+    cy.get('#password').type('123');
+    cy.get('#confirm_password').type('123');
+    cy.get('#pergunta').select('Aluno');
+    cy.get('.next-btn > .bx').click();
+    cy.get('#first_name').type('teste');
+    cy.get('#last_name').type('teste2');
+    cy.get('#CPF').type('11111111111');
+    cy.get('#telefone').type('11941500444');
+    cy.get('#data_nascimento').type('2005-11-05');
+    cy.get('#genero').select('Masculino');
+    cy.get('.next-btn > .bx').click();
+    cy.get('#CEP').type('54521150');
+    cy.get('#endereco').type('rua do teste');
+    cy.get('#numero').type('120');
+    cy.get('#bairro').type('bairro do teste');
+    cy.get('#estado').type('estado do teste');
+    cy.get('#estado').type('estado do teste');
+    cy.get('#cidade').type('cidade do teste');
+    cy.get('.next-btn > .bx').click();
+    cy.get('#escolaridade').select('Fundamental Completo');
+    cy.get('#escola').type('escola teste');
+    cy.get('#turno').select('Tarde');
+    cy.get('#curso').select('Iniciação a Programação');
+    cy.get('.form-step.active > .btn-2').click();
+});
+
+Cypress.Commands.add('createSuperUser', () => {
+    cy.visit('/auth/register/');
+    cy.get('#username').type('usuario teste');
+    cy.get('#email').type('teste@gmail.com');
+    cy.get('#password').type('123');
+    cy.get('#confirm_password').type('123');
+    cy.get('#pergunta').select('Aluno');
+    cy.get('#submit-professor').click();
+});
+
+Cypress.Commands.add('deleteAllUsers', () => {
+    cy.exec('python delete_user.py', { failOnNonZeroExit: false });
+});
+
+Cypress.Commands.add('deleteAllInformacoes', () => {
+    cy.exec('python delete_informacoes.py', { failOnNonZeroExit: false });
+});
+
+describe('teste de cadastro', () => {
+    beforeEach(() => {
+        cy.deleteAllUsers();
+        cy.deleteAllInformacoes();
+        
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // Ignora erros de elementos não encontrados
+            if (err.message.includes('document.getElementById') || 
+                err.message.includes('null') ||
+                err.message.includes('undefined')) {
+            return false
+            }
+            return true
+        })
+    });
+
+    it('', () => {
+      
+    });
+
+    it('', () => {
+        
+    });
+    
+    afterEach(() => {
+        cy.deleteAllCurso();
+        cy.deleteAllUsers();
+        cy.deleteAllInformacoes();
+    });   
+});
